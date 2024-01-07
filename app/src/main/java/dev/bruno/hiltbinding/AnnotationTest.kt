07f4f83e8a6b21de38914f8f3fc65c100614e3a8
Bruno Hensel <bruno.hensel@gmail.com>
2024-01-07 16:20:04 +0100

    add annotation and comment out manual binding

--- app/src/main/java/dev/bruno/hiltbinding/AnnotationTest.kt
+++ app/src/main/java/dev/bruno/hiltbinding/AnnotationTest.kt
@@ -1,11 +1,10 @@
 package dev.bruno.hiltbinding
 
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dev.bruno.annotation.ContributesBinding
 import javax.inject.Inject
 
+@ContributesBinding(component = ActivityComponent::class)
 class AnnotationTest @Inject constructor() : Test {
     override fun print(message: String) {
         println(message)
@@ -16,10 +15,10 @@ interface Test {
     fun print(message: String)
 }
 
-@Module
-@InstallIn(ActivityComponent::class)
-interface TestModule {
-
-    @Binds
-    fun bindTest(impl: AnnotationTest): Test
-}
+//@Module
+//@InstallIn(ActivityComponent::class)
+//interface TestModule {
+//
+//    @Binds
+//    fun bindTest(impl: AnnotationTest): Test
+//}
